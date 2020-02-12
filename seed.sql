750507cb05b3712adbcf7604a879384cbdacbd51
Catie Evans <catieevans@Caties-MacBook-Pro.local>
2020-02-11 19:48:57 -0500

    adds styling to event cards

--- seed.sql
+++ seed.sql
@@ -4,7 +4,7 @@ VALUES
     ('Travis', 'Franklin', 'DigitalCrafts', 'travis@gmail.com', '229-237-0490', 'catie_cat', 'bhdbdvsjaj');
 
 insert into events
-    (event_name, event_location, event_date, event_time, event_description, user_id)
+    (event_name, event_location, event_date, event_time, event_description, event_image, user_id)
 VALUES
     ('thanksgiving', 'my house', '2020-01-17', '05:00 PM', 'jobs for jobies', 1);
 
