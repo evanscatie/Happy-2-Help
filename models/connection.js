00ec63c4742639e0fe3765f5ff310ae4939fd869
Catie Evans <catieevans@Caties-MacBook-Pro.local>
2020-02-20 16:42:43 -0500

    adds prettier

--- models/connection.js
+++ models/connection.js
@@ -1,19 +1,14 @@
-const pgp = require('pg-promise')({
-    query: e => {
-        console.log(`QUERY: ${e.query}`);
-    }
+// --- Establishes Database connection
+const pgp = require("pg-promise")({
+	query: e => {
+		console.log(`QUERY: ${e.query}`);
+	}
 });
 
 const options = {
-    host: 'localhost',
-    database: 'to-do-app'
+	host: "localhost",
+	database: "to-do-app"
 };
 
 const db = pgp(options);
 module.exports = db;
-
-
-
-
-
-
--- public/scripts/createEvent.js
+++ public/scripts/createEvent.js
@@ -1,51 +1,51 @@
-// ADD TASKS    
+// ADD TASKS
 
-$(document).ready(function () {
-    var counter = 0;
+$(document).ready(function() {
+	var counter = 0;
 
-    $("#addrow").on("click", function () {
-        var newRow = $("<tr>");
-        var cols = "";
+	$("#addrow").on("click", function() {
+		var newRow = $("<tr>");
+		var cols = "";
 
-        cols += '<td class="borderless"><input type="text" class="form-control" name="taskList"/></td>';
+		cols +=
+			'<td class="borderless"><input type="text" class="form-control" name="taskList"/></td>';
 
-        cols += '<td class="borderless"><input type="button" class="ibtnDel btn btn-md btn-danger "  value="Delete"></td>';
-        newRow.append(cols);
-        $("table.order-list").append(newRow);
-        counter++;
-    });
+		cols +=
+			'<td class="borderless"><input type="button" class="ibtnDel btn btn-md btn-danger "  value="Delete"></td>';
+		newRow.append(cols);
+		$("table.order-list").append(newRow);
+		counter++;
+	});
 
-    $("table.order-list").on("click", ".ibtnDel", function (event) {
-        $(this).closest("tr").remove();       
-        counter -= 1
-    });
-
-
+	$("table.order-list").on("click", ".ibtnDel", function(event) {
+		$(this)
+			.closest("tr")
+			.remove();
+		counter -= 1;
+	});
 });
 
 function calculateRow(row) {
-    var price = +row.find('input[name^="price"]').val();
-
+	var price = +row.find('input[name^="price"]').val();
 }
 
 function calculateGrandTotal() {
-    var grandTotal = 0;
-    $("table.order-list").find('input[name^="price"]').each(function () {
-        grandTotal += +$(this).val();
-    });
-    $("#grandtotal").text(grandTotal.toFixed(2));
+	var grandTotal = 0;
+	$("table.order-list")
+		.find('input[name^="price"]')
+		.each(function() {
+			grandTotal += +$(this).val();
+		});
+	$("#grandtotal").text(grandTotal.toFixed(2));
 }
 
-
-
 // date pickers
-
-$(function () {
-    $('#datetimepicker1').datetimepicker();
+$(function() {
+	$("#datetimepicker1").datetimepicker();
 });
 
-$(function () {
-    $('#datetimepicker3').datetimepicker({
-        format: 'LT'
-    });
+$(function() {
+	$("#datetimepicker3").datetimepicker({
+		format: "LT"
+	});
 });
